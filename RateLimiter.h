be6dbbf13335fbb287b39c925541204be3a3a503
GoyalKrish <goyalkrish183@gmail.com>
2026-09-08 23:45:19 +0530

    added mutex lock for thread safety on tokens

--- RateLimiter.h
+++ RateLimiter.h
@@ -1,5 +1,6 @@
     #include<chrono>
     #include<algorithm>
+    #include<mutex>
     using namespace std;
 
     long long nowTime(){
@@ -11,6 +12,7 @@
         unsigned int capacity;
         unsigned int refill_rate;
         long long last_time;
+        mutex mtx;
 
         public:
             RateLimiter(int cap, int rate){
@@ -22,20 +24,17 @@
 
 
             bool allowRequest(){
-            // 1. Check how much time passed since last_time
-            long long now_time = nowTime();
-            long long diff = now_time - last_time;
-            last_time = now_time;
-            // 2. Calculate how many credits to add based on time passed
-            double canAdd = (diff / 1000.0) * refill_rate;
-            // 3. Add credits (but don't exceed capacity)
-            tokens = min((double)capacity, tokens + canAdd);
-            // 4. If tokens >= 1, use 1 token and return true
-            if(tokens >= 1){
-                --tokens;
-                return true;
-            }else return false;
-            // 5. Otherwise return false
+                lock_guard<mutex> lock(mtx);
+                long long now_time = nowTime();
+                long long diff = now_time - last_time;
+                last_time = now_time;
+                
+                double canAdd = (diff / 1000.0) * refill_rate;
+                tokens = min((double)capacity, tokens + canAdd);
 
+                if(tokens >= 1){
+                    --tokens;
+                    return true;
+                }else return false;
             }
     };
